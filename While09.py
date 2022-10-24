0f2fedcbb83a1de67df491257ecc9985f11eb825
Roma1125 <114662266+Roma1125@users.noreply.github.com>
2022-10-24 14:45:28 +0500

    fgffhghfgh
    bxxcvxcvx

--- While09.py
+++ While09.py
@@ -1,9 +1,16 @@
 def main(s):
     """
-    A string of numbers is given. Find the sum of all the numbers and return.
+    A string of numbers is given. Find how many even numbers there are and return.
     Args:
         s: str
     Returns:
         int: return answer
     """
-    return
+    t=0
+    i=0
+    while t<=len(s)-1:
+        if  int(s[t])%2==0:
+            i+=int(s[t])
+        t+=1
+    return i 
+print(main('12345678'))
