e73329475185d6607816e045ff7a54e065d194a4
Roma1125 <114662266+Roma1125@users.noreply.github.com>
2022-10-24 14:41:41 +0500

    sdadfdgdfhfgh
    bcvbcvbcx

--- While08.py
+++ While08.py
@@ -1,9 +1,16 @@
 def main(s):
     """
-    A string of numbers is given. Find how many odd numbers there are and return.
+    A string of numbers is given. Find how many even numbers there are and return.
     Args:
         s: str
     Returns:
         int: return answer
     """
-    return
+    t=0
+    i=0
+    while t<=len(s)-1:
+        if  int(s[t])%2==1:
+            i+=1
+        t+=1
+    return i 
+print(main('12345678'))
